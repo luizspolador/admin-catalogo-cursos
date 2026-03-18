a916647a94105b112586816fff7ca6aa2ffbfd8e
Luiz Spolador <luizh.spolador@gmail.com>
2026-03-18 17:25:07 -0300

    adicionando a camada de infraestrutura - spring framework, sql, h2, hibernate-jpa, configurando hikari e flyway

---
+++ infrastructure/src/main/java/com/spolador/admin/catalogo/infrastructure/configuration/WebServerConfig.java
@@ -0,0 +1,9 @@
+package com.spolador.admin.catalogo.infrastructure.configuration;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("com.spolador.admin.catalogo")
+public class WebServerConfig {
+}
